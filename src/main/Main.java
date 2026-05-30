3f562e422d8e37905dc014bf36158a5ca2326bfd
Joaquin <joaquinera12@gmail.com>
2026-05-29 23:37:41 -0300

    a

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,12 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // 1. Crear la Vista (La interfaz gráfica)
+            
             BMSDashboard dashboardView = new BMSDashboard();
             
-            // 2. Crear el Controlador
-            // Pasamos 'null' como batería inicial porque el Controlador ahora 
-            // crea la primera batería automáticamente con su propio botón.
             new BMSController(null, dashboardView);
             
-            // 3. Mostrar la ventana
-            dashboardView.setLocationRelativeTo(null); // Centrar en pantalla
+            dashboardView.setLocationRelativeTo(null); 
             dashboardView.setVisible(true);
         });
     }
